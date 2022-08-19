9f4963dabc1bed90a6410565c28c48d96bf33b9f
denisk13005 <kirmanndenis@gmail.com>
2022-08-19 18:46:32 +0200

    ras

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import Header from "../components/Header"
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.scss"
 import Product from "../components/Product"
 import { GiH2O } from "react-icons/gi"
 import { useState } from "react"
@@ -15,37 +15,20 @@ export default function Home({ best }) {
   //   setProducts(best.bestsellers.splice(0, 45));
   // }, []);
   return (
-    <div className={styles.container}>
+    <div className={styles.homeContainer}>
       <Head>
         <title>Amazon next clone</title>
         <meta name="description" content="Amazon clone project" />
         <link rel="icon" href="./favicon.png" />
       </Head>
-      {/* <main className={styles.homeMainContainer}>
-        <section className={styles.productsContainer}>
-          <div className={styles.productRow}>
-            {products &&
-              products.map((product, i) => (
-                <Product
-                  key={product.position}
-                  id={2}
-                  description={product.title}
-                  price={product.price.value}
-                  smallPrice={1}
-                  stars={3}
-                  img={product.image}
-                />
-              ))}
-
-          </div>
-        </section>
-      </main> */}
+      <section className={styles.galeryContainer}>galery container</section>
+      <section className={styles.productsContainer}>Products container</section>
     </div>
   )
 }
 export const getServerSideProps = async () => {
   const data = await fetch(
-    "https://testdeploynodedk.herokuapp.com/api/stuff"
+    "https://fakestoreapi.com/products/category/electronics"
   ).then((res) => res.json())
 
   return {
