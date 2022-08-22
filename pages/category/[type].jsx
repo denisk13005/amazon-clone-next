0e3845e23e6a978a7b97a9d855dcae3f5d516eaf
denisk13005 <kirmanndenis@gmail.com>
2022-08-22 11:23:45 +0200

    page peoduct ok

--- pages/category/[type].jsx
+++ pages/category/[type].jsx
@@ -4,6 +4,7 @@ import Layout from "./../../components/Layout"
 import Image from "next/image"
 
 const TypeOfCategory = ({ products }) => {
+  console.log(products)
   return (
     <section className={styles.typeOfCategoryBody}>
       <div className={styles.typeOfCategoryContainer}>
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,20 +1,53 @@
 import Image from "next/image"
 import React, { useState, useEffect } from "react"
-import styles from "../../styles/Product.module.scss"
+import styles from "../../styles/ProductPage.module.scss"
 import { useRouter } from "next/router"
 import { fetchData } from "next-auth/client/_utils"
 
 const Product = ({ product }) => {
   return (
-    <div className={styles.productContainer}>
-      <div className={styles.imgProductContainer}>
-        <Image
-          src={product && product.image}
-          layout="fill"
-          alt={product && product.title}
-        />
-      </div>
-    </div>
+    <>
+      {product && (
+        <main className={styles.productPageContainer}>
+          <div className={styles.leftProductContainer}>
+            <div className={styles.imgProductContainer}>
+              <Image src={product.image} layout="fill" alt={product.title} />
+            </div>
+          </div>
+          <div className={styles.rightDescriptioncontainer}>
+            <h3>{product.title}</h3>
+            <div className={styles.productRatingContainer}>
+              <span>{product.rating.rate}⭐</span>{" "}
+              <span>{product.rating.count} évaluations</span>
+            </div>
+            <p className={styles.productPrice}>{product.price} €</p>
+            <p className={styles.propos}>À propos de cet article :</p>
+            <p className={styles.productDesc}>{product.description}</p>
+          </div>
+          <aside className={styles.addProduct}>
+            <p className={styles.productPrice}>{product.price} €</p>
+            <div className={styles.shippingAddress}>addresse</div>
+            <p className={styles.inStock}>En stock</p>
+            <div className={styles.qte}>
+              Quantité :{" "}
+              <select className={styles.selectQte}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+            <button className={styles.addToBasket}>Ajouter au panier</button>
+          </aside>
+        </main>
+      )}
+    </>
   )
 }
 
