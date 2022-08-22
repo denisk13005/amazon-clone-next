0034fb522b6f6c21dfab229437d347d76e4fca64
denisk13005 <kirmanndenis@gmail.com>
2022-08-22 15:24:43 +0200

    me en place de redux du state et du slice products

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import Layout from "../components/Layout"
+import { Provider } from "react-redux"
+import { store } from "../redux/store"
 import { SessionProvider } from "next-auth/react"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps, session }) {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </Provider>
   )
 }
 
---
+++ redux/productsSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState: {
+    products: [],
+    orders: [],
+    basketItems: 0,
+    loading: false,
+    totalPrice: 0,
+  },
+  reducers: {
+    addProduct: (state, action) => {
+      const productPresent = state.products.find(
+        (el) => el.id === action.payload.id
+      )
+      if (productPresent) {
+        // if the product is already in the basket, we just add the quantity
+        state.products.forEach((product) => {
+          if (product.id === action.payload.id) {
+            product.qte++
+          }
+        })
+      }
+      // if the product is not in the basket, we add it
+      else {
+        let product = action.payload
+        product.qte = 1
+        state.products = [...state.products, product]
+      }
+      //on incrémente le nb d'items dans le panier
+      state.basketItems++
+      //on met à jour le total
+      const price = `${action.payload.price}.${action.payload.smallPrice}`
+      state.totalPrice = state.totalPrice + parseFloat(price)
+    },
+  },
+})
+export const { addProduct } = productsSlice.actions
+export default productsSlice.reducer
