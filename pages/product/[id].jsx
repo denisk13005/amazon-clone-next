fb0f8f81216e18a04b6a9c0fd2321a5e5b4168b3
denisk13005 <kirmanndenis@gmail.com>
2022-08-22 09:06:37 +0200

    test getStaticPath

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,25 +4,7 @@ import styles from "../../styles/Product.module.scss"
 import { useRouter } from "next/router"
 import { fetchData } from "next-auth/client/_utils"
 
-const Product = () => {
-  const [product, setProduct] = useState({})
-  const router = useRouter()
-  const id = parseInt(router.query.id)
-
-  const url = `https://fakestoreapi.com/products/${id}`
-  console.log(typeof id)
-
-  useEffect(() => {
-    console.log("test")
-    const fetchData = async (id) => {
-      const data = await fetch(url)
-        .then((res) => res.json())
-        .then((data) => data)
-      console.log(data)
-      setProduct(data)
-    }
-    fetchData()
-  }, [id, url])
+const Product = ({ product }) => {
   return (
     <div className={styles.productContainer}>
       <div className={styles.imgProductContainer}>
@@ -31,15 +13,27 @@ const Product = () => {
     </div>
   )
 }
-// export const getServerSideProps = async ({ params }) => {
-//   const id = params.id
-// const product = await fetch(`https://fakestoreapi.com/products/${id}`).then(
-//     (res) => res.json()
-//   )
-//   return {
-//     props: {
-//       product,
-//     },
-//   }
-// }
+
+export const getStaticPaths = async () => {
+  const datas = await fetch(`https://fakestoreapi.com/products`).then((res) =>
+    res.json()
+  )
+
+  const paths = datas.map((data) => ({
+    params: { id: data.id.toString() },
+  }))
+  return { paths, fallback: true }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const id = params.id
+  const product = await fetch(`https://fakestoreapi.com/products/${id}`).then(
+    (res) => res.json()
+  )
+  return {
+    props: {
+      product,
+    },
+  }
+}
 export default Product
