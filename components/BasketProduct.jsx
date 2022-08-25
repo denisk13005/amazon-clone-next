8c5a2286be001aca863295d585c1eaec8b9b5ef7
denisk13005 <kirmanndenis@gmail.com>
2022-08-25 17:41:53 +0200

    basketPage section produits ok

---
+++ components/BasketProduct.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import styles from "../styles/basketProduct.module.scss"
+import Image from "next/image"
+
+const BasketProduct = ({ product }) => {
+  return (
+    <div className={styles.basketProductContainer}>
+      <div className={styles.leftSection}>
+        <div className={styles.imgAndDesc}>
+          <div className={styles.imgContainer}>
+            <Image
+              src={product.image}
+              layout="fill"
+              alt={`image of ${product.title}`}
+              objectFit="contain"
+            />
+          </div>
+          <div className={styles.descContainer}>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>Qté : {product.qte}</p>
+          </div>
+        </div>
+
+        <div className={styles.priceContainer}>{product.price} €</div>
+      </div>
+      <div className={styles.rightSection}></div>
+    </div>
+  )
+}
+
+export default BasketProduct
--- pages/basket.jsx
+++ pages/basket.jsx
@@ -3,9 +3,11 @@ import Image from "next/image"
 import Link from "next/link"
 import styles from "../styles/BasketPage.module.scss"
 import { useSelector } from "react-redux"
+import BasketProduct from "../components/BasketProduct"
 
 const Basket = () => {
   const products = useSelector((state) => state.products.products)
+  const productsState = useSelector((state) => state.products)
   console.log(products)
   return products.length === 0 ? (
     <main className={styles.emptyBasketMain}>
@@ -25,7 +27,30 @@ const Basket = () => {
     </main>
   ) : (
     <main className={styles.basketPageMain}>
-      <section className={styles.productsContainer}></section>
+      <section className={styles.productsContainer}>
+        <div className={styles.header}>
+          <h2>Votre Panier</h2>
+          <span>Prix</span>
+        </div>
+
+        {products.map((product) => (
+          <>
+            <BasketProduct product={product} />
+          </>
+        ))}
+        <div className={styles.subtotal}>
+          <p>
+            Sous-total{" "}
+            {`( ${productsState.basketItems} ${
+              productsState.basketItems == 1 ? "article" : "articles"
+            })`}
+            :
+            <span className={styles.subtotalPrice}>
+              {productsState.totalPrice} €
+            </span>
+          </p>
+        </div>
+      </section>
       <section className={styles.rightSection}></section>
     </main>
   )
